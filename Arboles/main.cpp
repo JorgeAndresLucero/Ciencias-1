fe804213b0d011257e3d922b1988e418e03cdbdc
JorgeAndresLucero <jorgeandres31@gmail.com>
2018-05-05 19:41:17 -0500

    Add files via upload

--- Arboles/main.cpp
+++ Arboles/main.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 #include <stdlib.h>
 #include <math.h> /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-using namespace std; 
-//void intercambiar( int p, int i, int a, );
-int main(int argc, char** argv) { 	
-int tam=0; 	
-int i=0; 	
-int aux=0; 	
-int padre=0; 	
-int prior; 	
-cout<<"Ingrese el tamanio de la cola"<<endl; 	
-cin>>tam; 	
-tam++; 	
-int arr[tam]; 	
-arr[0]=0; 	 	 	
-for(i=1; i<tam;i++){ 		
-cout<<"Ingrese el valor de prioridad "<<i<<endl; 		
-cin>>prior; 		
-arr[i]=prior; 		
-if(i != 1){ 	
-padre = i/2; 	
+	using namespace std; 
+	//void intercambiar( int p, int i, int a, );
+	int main(int argc, char** argv) { 	
+	int tam=0; 	
+	int i=0; 	
+	int aux=0; 	
+	int padre=0; 	
+	int prior=0; 	
+	int opcion=0;
+	int elimina=0;
+	
+//	do{ hacer que esto funcione bien bonito por fa
+	cout<<"Bienvenido, por favor elija una opcion"<<endl;
+	cout<<" 1. Insertar una cola de prioridad"<<endl;
+	cout<<" 2. Eliminar una posicion"<<endl;
+	cout<< "3. SALIR "<<endl;
+	cin>>opcion;
+
+   cout<<"Ingrese el tamanio de la cola"<<endl; 	
+     cin>>tam; 	
+		tam++; 	
+		int arr[tam]; 	
+		arr[0]=0; 	 
+   switch(opcion){
+		case 1 :{
+		 	
+		for(i=1; i<tam;i++){ 		
+		cout<<"Ingrese el valor de prioridad "<<i<<endl; 		
+		cin>>prior; 		
+		arr[i]=prior; 		
+		if(i != 1){ 	
+		padre = i/2; 	
 		if(arr[i] > arr[padre]){ 
 		//intercambiar(padre,i,aux,arr);			//se realiza el intercambio 	
 			aux=arr[i]; 	
@@ -33,8 +46,7 @@ padre = i/2;
 		// }	
 				
 		}
-					
-			 		
+		 		
 			} 	
 			 } 	else{ 		
 			 	padre = (i+1)/2; 	
@@ -45,9 +57,31 @@ padre = i/2;
 			 	for(i=1; i<tam; i++){ 		 	
 			 		cout<<" "<<arr[i]; 	
 			 		cout<<" "; 	
-			 		} 	 
-			 		return 0;
+			 		} 
+          break;
+      }
+     
+//-----------------------Elminar----------------------------------------------------------
+case 2: {
+	//aqui va la logica :P
+	cout<<"Ingrese la prioridad que desea eliminar"<<endl;
+	cin>>elimina;
+	for(i=1; i<tam;i++){
+	if(arr[i] == elimina){
+		arr[i]= 0;
+	}	
+	}
+	break;
 }
+	
+	default:
+	cout<<"Dato invalido!!! "<<endl;
+	break;
+}
+//while(opcion != 3);arreglar esto 
+	return 0;
+}
+
 /*
 void intercambiar (int p,int i, int a){
             aux=arr[i]; 	
