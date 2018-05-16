10a4094252fb165bde4b5d2ce5cfb35eef8d044b
Juliana Capador <jalexaco@gmail.com>
2018-05-16 09:10:59 -0500

    Add files via upload

--- FINAL/main.cpp
+++ FINAL/main.cpp
@@ -12,8 +12,34 @@ using namespace std;
 	2. Determinar la cantidad de votantes, ya sabemos que no toca clasificarlos por género
 	3. Como ya se tendrían los datos de los Candidatos organizados, entonces ya se pueden generar los resultados
 	4. Entonces mirar si manejar la misma estructura para las de alcaldía y presidenciales, porque los resultados los queremos observar por separado.
+	5. De acuerdo con el artículo 190 de la Constitución Política de Colombia el presidente de la república será elegido por la mitad más uno de los votos que depositen los ciudadanos.
 */
 int main(int argc, char** argv) {
+	
+	//Lectura del archivo
+	
+	//ALCALDIAS
+	//Reporte General
+	int totalVotosDepto;
+	int votosBlanco;
+	int votosNulo;
+	int votosAbstencion;
+	
+	//Reporte Detallado
+	int alcaldesPartidoDepto;
+	int hombres;
+	int mujeres;
+	
+	//Reporte Total
+	int alcaldesPartido;
+	
+	//PRESIDENTE 
+	int pVotosDepto;
+	int pBlancoDepto;
+	int pNuloDepto;
+	int pAbstencionDepto;
+	
+	//
 	
 	LinkedList <int> vCandidatos;
 	
